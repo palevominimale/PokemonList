c8a218781eb44bd8cd9a9f24974bef17230db2ec
Valerii Makarov <badtri.orchestra@gmail.com>
2022-10-14 18:53:23 +0200

    added entities to DB according to POJO

--- app/src/main/java/app/seals/pokemonlist/data/models/PokemonDataModel.kt
+++ app/src/main/java/app/seals/pokemonlist/data/models/PokemonDataModel.kt
@@ -3,7 +3,6 @@ package app.seals.pokemonlist.data.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import app.seals.pokemonlist.domain.models.SpritesDomainModel
 import java.io.Serializable
 
 @Entity(tableName = "Pokemons")
@@ -13,5 +12,5 @@ data class PokemonDataModel(
     @ColumnInfo(name="name"     )   var name                   : String?               = null,
     @ColumnInfo(name="types"    )   var types                  : ArrayList<String>     = arrayListOf(),
     @ColumnInfo(name="weight"   )   var weight                 : Int?                  = null,
-    @ColumnInfo(name="sprites"  )   var sprites                : SpritesDomainModel?   = SpritesDomainModel(),
+    @ColumnInfo(name="sprites"  )   var sprites                : SpritesDataModel?   = SpritesDataModel(),
 ) : Serializable
