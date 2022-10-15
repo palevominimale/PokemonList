f01f16f2a79d61e89d65eefeb1484607884b9ede
Valerii Makarov <badtri.orchestra@gmail.com>
2022-10-15 12:15:10 +0200

    removed extra logs

--- app/src/main/java/app/seals/pokemonlist/ui/adapters/RecyclerAdapter.kt
+++ app/src/main/java/app/seals/pokemonlist/ui/adapters/RecyclerAdapter.kt
@@ -1,17 +1,13 @@
 package app.seals.pokemonlist.ui.adapters
 
-import android.graphics.Bitmap
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import app.seals.pokemonlist.R
 import app.seals.pokemonlist.domain.models.PokemonListDomainModel
-import com.squareup.picasso.Picasso
 
 class RecyclerAdapter(
     private val data: MutableLiveData<PokemonListDomainModel>
@@ -30,7 +26,6 @@ class RecyclerAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val name = data.value?.results?.get(position)?.name
         holder.name.text = name
-        Log.e("RA", "$position $name")
     }
 
     override fun getItemCount(): Int = data.value?.results?.size ?: 0
